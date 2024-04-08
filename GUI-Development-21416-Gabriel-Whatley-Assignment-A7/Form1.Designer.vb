a438c3a2a89a4a702e4d89360a860f15a61bde59
Gabriel Whatley <whatley_gabriel@students.columbusstate.edu>
2024-04-08 18:24:55 -0400

    Layout and controls complete.

--- GUI-Development-21416-Gabriel-Whatley-Assignment-A7/Form1.Designer.vb
+++ GUI-Development-21416-Gabriel-Whatley-Assignment-A7/Form1.Designer.vb
@@ -22,10 +22,105 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        PictureBox1 = New PictureBox()
+        Label1 = New Label()
+        ComboBox1 = New ComboBox()
+        Label2 = New Label()
+        Button1 = New Button()
+        Label3 = New Label()
+        lbl_largest_savings = New Label()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.smarthome
+        PictureBox1.Location = New Point(12, 12)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(374, 264)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(541, 51)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(88, 30)
+        Label1.TabIndex = 1
+        Label1.Text = "Smart Home" & vbCrLf & "Electric Savings"
+        Label1.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(538, 134)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 2
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(292, 306)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(144, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "The electric savings for  is:"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(337, 324)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(140, 23)
+        Button1.TabIndex = 4
+        Button1.Text = "Display Statistics"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(311, 360)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(166, 15)
+        Label3.TabIndex = 5
+        Label3.Text = "The average monthly savings: "
+        ' 
+        ' lbl_largest_savings
+        ' 
+        lbl_largest_savings.AutoSize = True
+        lbl_largest_savings.Location = New Point(314, 390)
+        lbl_largest_savings.Name = "lbl_largest_savings"
+        lbl_largest_savings.Size = New Size(177, 15)
+        lbl_largest_savings.TabIndex = 6
+        lbl_largest_savings.Text = "had the most significant savings"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.AntiqueWhite
+        ClientSize = New Size(800, 450)
+        Controls.Add(lbl_largest_savings)
+        Controls.Add(Label3)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(ComboBox1)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Fart Home Application"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lbl_largest_savings As Label
 
 End Class
