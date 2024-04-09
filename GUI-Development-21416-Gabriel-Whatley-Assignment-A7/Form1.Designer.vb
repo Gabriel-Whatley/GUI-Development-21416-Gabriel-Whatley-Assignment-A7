565a0d0ef4f99dd15219981a6345600b4451f21b
Gabriel Whatley <whatley_gabriel@students.columbusstate.edu>
2024-04-09 14:09:34 -0400

    Completed parsing text file into arrays, use array to populate combobox.

--- GUI-Development-21416-Gabriel-Whatley-Assignment-A7/Form1.Designer.vb
+++ GUI-Development-21416-Gabriel-Whatley-Assignment-A7/Form1.Designer.vb
@@ -24,10 +24,10 @@ Partial Class Form1
     Private Sub InitializeComponent()
         PictureBox1 = New PictureBox()
         Label1 = New Label()
-        ComboBox1 = New ComboBox()
-        Label2 = New Label()
-        Button1 = New Button()
-        Label3 = New Label()
+        cb_month = New ComboBox()
+        lbl_selected_month_savings = New Label()
+        btn_display_stats = New Button()
+        lbl_average_savings = New Label()
         lbl_largest_savings = New Label()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
@@ -52,61 +52,71 @@ Partial Class Form1
         Label1.Text = "Smart Home" & vbCrLf & "Electric Savings"
         Label1.TextAlign = ContentAlignment.TopCenter
         ' 
-        ' ComboBox1
+        ' cb_month
         ' 
-        ComboBox1.FormattingEnabled = True
-        ComboBox1.Location = New Point(525, 166)
-        ComboBox1.Name = "ComboBox1"
-        ComboBox1.Size = New Size(121, 23)
-        ComboBox1.TabIndex = 2
+        cb_month.FormattingEnabled = True
+        cb_month.Location = New Point(525, 166)
+        cb_month.Name = "cb_month"
+        cb_month.Size = New Size(121, 23)
+        cb_month.TabIndex = 2
         ' 
-        ' Label2
+        ' lbl_selected_month_savings
         ' 
-        Label2.AutoSize = True
-        Label2.Location = New Point(337, 297)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(144, 15)
-        Label2.TabIndex = 3
-        Label2.Text = "The electric savings for  is:"
+        lbl_selected_month_savings.AutoSize = True
+        lbl_selected_month_savings.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lbl_selected_month_savings.Location = New Point(282, 314)
+        lbl_selected_month_savings.Name = "lbl_selected_month_savings"
+        lbl_selected_month_savings.Size = New Size(271, 30)
+        lbl_selected_month_savings.TabIndex = 3
+        lbl_selected_month_savings.Text = "The electric savings for  is:"
+        lbl_selected_month_savings.TextAlign = ContentAlignment.MiddleCenter
+        lbl_selected_month_savings.Visible = False
         ' 
-        ' Button1
+        ' btn_display_stats
         ' 
-        Button1.Location = New Point(337, 324)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(140, 23)
-        Button1.TabIndex = 4
-        Button1.Text = "Display Statistics"
-        Button1.UseVisualStyleBackColor = True
+        btn_display_stats.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold)
+        btn_display_stats.Location = New Point(298, 347)
+        btn_display_stats.Name = "btn_display_stats"
+        btn_display_stats.Size = New Size(239, 39)
+        btn_display_stats.TabIndex = 4
+        btn_display_stats.Text = "Display Statistics"
+        btn_display_stats.UseVisualStyleBackColor = True
         ' 
-        ' Label3
+        ' lbl_average_savings
         ' 
-        Label3.AutoSize = True
-        Label3.Location = New Point(325, 361)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(166, 15)
-        Label3.TabIndex = 5
-        Label3.Text = "The average monthly savings: "
+        lbl_average_savings.AutoSize = True
+        lbl_average_savings.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold)
+        lbl_average_savings.Location = New Point(262, 389)
+        lbl_average_savings.Name = "lbl_average_savings"
+        lbl_average_savings.Size = New Size(310, 30)
+        lbl_average_savings.TabIndex = 5
+        lbl_average_savings.Text = "The average monthly savings: "
+        lbl_average_savings.TextAlign = ContentAlignment.MiddleCenter
+        lbl_average_savings.Visible = False
         ' 
         ' lbl_largest_savings
         ' 
         lbl_largest_savings.AutoSize = True
-        lbl_largest_savings.Location = New Point(314, 390)
+        lbl_largest_savings.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold)
+        lbl_largest_savings.Location = New Point(208, 428)
         lbl_largest_savings.Name = "lbl_largest_savings"
-        lbl_largest_savings.Size = New Size(225, 15)
+        lbl_largest_savings.Size = New Size(419, 30)
         lbl_largest_savings.TabIndex = 6
         lbl_largest_savings.Text = "had the most significant monthly savings"
+        lbl_largest_savings.TextAlign = ContentAlignment.MiddleCenter
+        lbl_largest_savings.Visible = False
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.AntiqueWhite
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(800, 511)
         Controls.Add(lbl_largest_savings)
-        Controls.Add(Label3)
-        Controls.Add(Button1)
-        Controls.Add(Label2)
-        Controls.Add(ComboBox1)
+        Controls.Add(lbl_average_savings)
+        Controls.Add(btn_display_stats)
+        Controls.Add(lbl_selected_month_savings)
+        Controls.Add(cb_month)
         Controls.Add(Label1)
         Controls.Add(PictureBox1)
         Name = "Form1"
@@ -118,10 +128,10 @@ Partial Class Form1
 
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents ComboBox1 As ComboBox
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Label3 As Label
+    Friend WithEvents cb_month As ComboBox
+    Friend WithEvents lbl_selected_month_savings As Label
+    Friend WithEvents btn_display_stats As Button
+    Friend WithEvents lbl_average_savings As Label
     Friend WithEvents lbl_largest_savings As Label
 
 End Class
--- GUI-Development-21416-Gabriel-Whatley-Assignment-A7/Form1.vb
+++ GUI-Development-21416-Gabriel-Whatley-Assignment-A7/Form1.vb
@@ -1,28 +1,42 @@
-﻿﻿'Name: Gabriel Whatley
-'Date : 4/9/24
+﻿'Name: Gabriel Whatley
+'Date: 4/9/24
 'Class: CPSC 3118 - Graphical User Interface Development - CRN:21416
 'Instructor: Bruce Montgomery
 'Program Description: Program that reads savings information from a file and displays the information to the user based on their choice of month.
 Public Class Form1
 
-    Dim file_reader As IO.StreamReader
-    Dim month_data As Array
+    Dim month_array(11) As String
+    Dim cost_array(11) As Decimal
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim file_contents As Array
+        Dim file_contents_index As Int16 = 0
+
         If IO.File.Exists("savings.txt") = True Then
-            file_reader = IO.File.OpenText("savings.txt")
+            file_contents = My.Computer.FileSystem.ReadAllText("savings.txt").Split(Environment.NewLine.ToArray) ' Read the entire file into an array split by newline characters.
         Else
             MsgBox("Make sure the savings.txt file is in the program directory")
             Close()
         End If
 
+        For index As Int16 = 0 To 11
+            month_array(index) = file_contents(file_contents_index)
+            cost_array(index) = file_contents(file_contents_index + 1)
+            file_contents_index += 2
+        Next
+
+        cb_month.Items.AddRange(month_array)
 
     End Sub
 
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cb_month.SelectedIndexChanged
+        lbl_selected_month_savings.Text = "The electric savings for " + month_array(cb_month.SelectedIndex) + " is : " + FormatCurrency(cost_array(cb_month.SelectedIndex), 2)
+        lbl_selected_month_savings.Visible = True
     End Sub
 
-
+    Private Sub btn_display_stats_Click(sender As Object, e As EventArgs) Handles btn_display_stats.Click
+        lbl_average_savings.Visible = True
+        lbl_largest_savings.Visible = True
+    End Sub
 End Class
